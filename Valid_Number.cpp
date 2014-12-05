a5b8bc7348fe4eafd2dbe005e1224a1e9a92fa44
NicGanon <NicGanon@gmail.com>
2014-12-05 20:20:40 +0800

    Update Valid_Number.cpp

--- Valid_Number.cpp
+++ Valid_Number.cpp
@@ -1,10 +1,10 @@
 
 bool isNumber(const char *s)
 {
-		if (s == NULL)
-				return false;
-		int i = 0;
-		while (i < strlen(s)) {
+	if (s == NULL)
+		return false;
+	int i = 0;
+	while (i < strlen(s)) {
 		    
-		}
+	}
 }
