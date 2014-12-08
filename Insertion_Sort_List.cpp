e7fbc61a23d502128f00c66c217433ad22e34bf6
NicGanon <NicGanon@gmail.com>
2014-12-08 21:43:13 +0800

    Update Insertion_Sort_List.cpp

--- Insertion_Sort_List.cpp
+++ Insertion_Sort_List.cpp
@@ -3,37 +3,37 @@ using namespace std;
 
 struct ListNode {
     int val;
-		ListNode *next;
-		ListNode(int x) : val(x),next(NULL) {}
+    ListNode *next;
+    ListNode(int x) : val(x),next(NULL) {}
 };
 
 ListNode *insertion(ListNode *head)
 {
-		if (head == NULL)
-				return NULL;
-		ListNode *newHead = head;
-		head = head->next;
-		newHead->next = NULL;
+	if (head == NULL)
+		return NULL;
+	ListNode *newHead = head;
+	head = head->next;
+	newHead->next = NULL;
 
-		while (head != NULL) {
-				ListNode *curNode = head;
-				ListNode *nextHead = head->next;
-				ListNode *curSort = newHead;
+	while (head != NULL) {
+		ListNode *curNode = head;
+		ListNode *nextHead = head->next;
+		ListNode *curSort = newHead;
 
-				while (curSort->next) {
-				     if (curNode->val < curSort->next->val)
-								 break;
-				     curSort = curSort->next;
-				}
-				if (curSort == newHead && curNode->val < curSort->val) {
-				    curNode->next = curSort;
-						newHead = curNode;
-				} else {
-				    curNode->next = curSort->next;
-						curSort->next = curNode;
-				}
-				head = nextHead;
-
+		while (curSort->next) {
+			if (curNode->val < curSort->next->val)
+				break;
+			curSort = curSort->next;
 		}
-		return newHead;
+		if (curSort == newHead && curNode->val < curSort->val) {
+			curNode->next = curSort;
+			newHead = curNode;
+		} else {
+		        curNode->next = curSort->next;
+			curSort->next = curNode;
+		}
+		head = nextHead;
+
+	}
+	return newHead;
 }
